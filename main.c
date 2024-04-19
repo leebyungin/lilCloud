40609acd615a1df17d767e55f701ae5a1798a67e
이병인 <0820b@naver.com>
2024-04-19 18:56:41 +0900

    main.c: SIGCHLD핸들러 추가

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
 
+void sigchldHandler(int signal);
+
 int main()
 {
     pid_t spid, gpid, ipid, wpid;
     int status, savedErrno;
+    struct sigaction sa;
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = sigchldHandler;
+    if(sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
+        printf("sigaction() failed\n");
+    }
 
     printf("main() 프로세스 시작\n");
     printf("sytem_server() 호출\n");
@@ -27,4 +39,9 @@ int main()
     waitpid(wpid, &status, 0);
 
     return 0;
+}
+
+void sigchldHandler(int signal)
+{
+    printf("main: Caught SIGCHLD:%d\n",signal);
 }
