634b6ef54f8023d45b339f4139944521d3eb8b9a
이병인 <0820b@naver.com>
2024-04-23 02:30:04 +0900

    Update main.c: printf() -> pMessage()로 교체
    message.h 추가

--- main.c
+++ main.c
@@ -6,8 +6,12 @@
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
+#include <message.h>
 
 void sigchldHandler(int signal);
+
+static const char *moduleName= "main";
+static pid_t globalPid=-1;
 
 int main()
 {
@@ -15,22 +19,25 @@ int main()
     int status, savedErrno;
     struct sigaction sa;
 
+    globalPid= getpid();
+
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     sa.sa_handler = sigchldHandler;
     if(sigaction(SIGCHLD, &sa, NULL) == -1)
     {
-        printf("sigaction() failed\n");
+        pMessage(moduleName, globalPid, "sigaction() failed");
+        return -1;
     }
 
-    printf("main() 프로세스 시작\n");
-    printf("sytem_server() 호출\n");
+    pMessage(moduleName, globalPid, "Starting");
+    pMessage(moduleName, globalPid, "Call system_server()");
     spid = create_system_server();
-    printf("web_server() 호출\n");
+    pMessage(moduleName, globalPid, "call web_server()");
     wpid = create_web_server();
-    printf("input() 호출\n");
+    pMessage(moduleName, globalPid, "call input()");
     ipid = create_input();
-    printf("gui() 호출\n");
+    pMessage(moduleName, globalPid, "call gui()");
     gpid = create_gui();
 
     waitpid(spid, &status, 0);
@@ -43,5 +50,12 @@ int main()
 
 void sigchldHandler(int signal)
 {
-    printf("main: Caught SIGCHLD:%d\n",signal);
+    pid_t childPid;
+    int status;
+
+    childPid= waitpid(-1, &status, WNOHANG);
+    printf("main.handler : Caught SIGCHLD(%d)\n",signal);
+    //pMessage(moduleName, globalPid, "Caught SIGCHLD(%d)",signal);
+    printf("main.handler : Reaped chld(%d)\n",childPid);
+    //pMessage(moduleName, globalPid, "Reaped child(%d)",childPid);
 }
