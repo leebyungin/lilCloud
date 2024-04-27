51d99064d1890d34cf7bba22685e2b43dc48a56c
이병인 <0820b@naver.com>
2024-04-28 01:08:06 +0900

    message.h: pMessage() 수정

--- message/message.h
+++ message/message.h
@@ -3,7 +3,7 @@
 
 #define ERROR_BUF_SIZE 15
 
-int pMessage(const char *name, pid_t pid, const char *message, ...);
+int pMessage(const char *message, ...);
 void error_handler(const char* msg, int bt);
 void perror_handler(const char* msg, int bt);
 void backtrace_log(void);
