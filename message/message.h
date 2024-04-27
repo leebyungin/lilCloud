bf541e2223ee944b672b71f81be945519b7ed494
이병인 <0820b@naver.com>
2024-04-28 00:11:38 +0900

    message.h 기능추가

--- message/message.h
+++ message/message.h
@@ -1,6 +1,10 @@
 #ifndef _MESSAGE_H_
 #define _MESSAGE_H_
 
-int pMessage(const char *name, pid_t pid, const char *message, ...);
+#define ERROR_BUF_SIZE 15
 
+int pMessage(const char *name, pid_t pid, const char *message, ...);
+void error_handler(const char* msg, int bt);
+void perror_handler(const char* msg, int bt);
+void backtrace_log(void);
 #endif
