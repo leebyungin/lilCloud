f10607ebd2c1c932d8eae494b8ab0e328d398f13
이병인 <0820b@naver.com>
2024-05-05 23:50:47 +0900

    [FIX] sigchldHandler() 내부 수정
    - fork() 종료 대기를 위해 waitpid() 호출 시 waitpid()와 sigchldHnaler() 둘 다 호출 되는 문제 해결
    - pMessage() 가변인자 구현으로 인한 printf() 제거

--- main.c
+++ main.c
@@ -10,69 +10,86 @@
 
 void sigchldHandler(int signal);
 
-static const char *moduleName= "main";
-static pid_t globalPid=-1;
+static const char *moduleName = "main";
+static pid_t globalPid = -1;
 
-static int processCount=0;
+static volatile int processCount = 0;
 
 int main()
 {
-    pid_t spid, gpid, ipid, wpid;
-    int status, savedErrno;
-    struct sigaction sa;
+	pid_t spid, gpid, ipid, wpid;
+	int status, savedErrno;
+	struct sigaction sa;
 
-    globalPid= getpid();
+	globalPid = getpid();
 
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sa.sa_handler = sigchldHandler;
-    if(sigaction(SIGCHLD, &sa, NULL) == -1)
-    {
-        pMessage("sigaction() failed");
-        return -1;
-    }
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = sigchldHandler;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1)
+	{
+		pMessage("sigaction() failed");
+		return -1;
+	}
 
-    pMessage("Starting");
-    pMessage("call system_server()");
-    spid = create_system_server();
+	pMessage("Starting");
+	pMessage("call system_server()");
+	spid = create_system_server();
 	processCount++;
 	
-    pMessage("call web_server()");
-    wpid = create_web_server();
+	pMessage("call web_server()");
+	wpid = create_web_server();
 	processCount++;
 	
-    pMessage("call input()");
-    ipid = create_input();
+	pMessage("call input()");
+	ipid = create_input();
 	processCount++;
 	
-    pMessage("call gui()");
-    gpid = create_gui();
+	pMessage("call gui()");
+	gpid = create_gui();
 	processCount++;
+	
 
-	// To-do : pid 리스트를 관리하는 기능 만들기.
-	// 현재 방식은 프로세스의 종료 순서가 고정돼 있음.
-    waitpid(spid, &status, 0);
-    waitpid(gpid, &status, 0);
-    waitpid(ipid, &status, 0);
-    waitpid(wpid, &status, 0);
+	// issue: 현재 방식은 프로세스의 종료 순서가 고정돼 있음.
+	/*
+	waitpid(spid, &status, 0);
+	waitpid(gpid, &status, 0);
+	waitpid(ipid, &status, 0);
+	waitpid(wpid, &status, 0);
+	*/
 
-	while(processCount>0)
+	while (processCount > 0)
 	{
 		pause();
 	};
 
-    return 0;
+	return 0;
 }
 
 void sigchldHandler(int signal)
 {
-    pid_t childPid;
-    int status;
+	pid_t childPid;
+	int status;
+	int savederrno = errno = 0;
 
-    childPid= waitpid(-1, &status, WNOHANG);
-    printf("main.handler : Caught SIGCHLD(%d)\n",signal);
-    //pMessage(moduleName, globalPid, "Caught SIGCHLD(%d)",signal);
-    printf("main.handler : Reaped chld(%d)\n",childPid);
-    //pMessage(moduleName, globalPid, "Reaped child(%d)",childPid);
+	childPid = waitpid(-1, &status, WNOHANG);
+	switch (childPid)
+	{
+	case -1:
+		if (errno != ECHILD)
+		{
+			perror_handler("sigchldHandler()", 0);
+		}
+		break;
+	case 0:
+		perror_handler("Child not changed. but SIGCHLD?", 0);
+		break;
+	default:
+		pMessage("Caught SIGCHLD(%d)", signal);
+		pMessage("Reaped child(%d)", childPid);
+		break;
+	}
 	processCount--;
+
+	errno = savederrno;
 }
